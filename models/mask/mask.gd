b3b06339cd437d4ef64afa18b9bfedb46db5e56d
Thomas Page <thomaspage33@gmail.com>
2026-01-31 10:29:01 +1300

    Mask disappears on collection

--- models/mask/mask.gd
+++ models/mask/mask.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
-	print("pick up mask")
-	GameManager.pickup_mask(Types.Mask.BLUE)
+	if $ColorRect.visible:
+		GameManager.pickup_mask(Types.Mask.BLUE)
+		$ColorRect.visible = false
