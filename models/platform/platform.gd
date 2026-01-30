ff1d91ebc7cb2b7510ed915d1ce143e7133682db
Thomas Page <thomaspage33@gmail.com>
2026-01-31 11:02:38 +1300

    simplify platform object

--- models/platform/platform.gd
+++ models/platform/platform.gd
@@ -1,5 +1,5 @@
 @tool
-extends Node2D
+extends StaticBody2D
 
 @export var platform_type := Types.Mask.NONE:
 	set(value):
@@ -15,25 +15,25 @@ var shape: Shape2D = RectangleShape2D.new()
 
 func _ready() -> void:
 	shape.size = size
-	$StaticBody2D/CollisionShape2D.shape = shape
+	$CollisionShape2D.shape = shape
 	_update_platform()
 
 func _update_platform():
 	if not is_inside_tree():
 		return
 	
-	$StaticBody2D/ColorRect.size = size
-	$StaticBody2D/ColorRect.position = Vector2(-size.x / 2, -size.y / 2)
+	$ColorRect.size = size
+	$ColorRect.position = Vector2(-size.x / 2, -size.y / 2)
 	
 	match platform_type:
 		Types.Mask.NONE:
-			$StaticBody2D.collision_layer = 1
-			$StaticBody2D/ColorRect.color = Color.WHITE
+			collision_layer = 1
+			$ColorRect.color = Color.WHITE
 		Types.Mask.BLUE:
-			$StaticBody2D.collision_layer = 2
-			$StaticBody2D/ColorRect.color = Color.BLUE
+			collision_layer = 2
+			$ColorRect.color = Color.BLUE
 		Types.Mask.RED:
-			$StaticBody2D.collision_layer = 4
-			$StaticBody2D/ColorRect.color = Color.RED
+			collision_layer = 4
+			$ColorRect.color = Color.RED
 	
 	shape.size = size
