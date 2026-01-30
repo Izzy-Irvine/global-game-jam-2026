6fedbbd08f7323af4288fb5fd2600666661acd52
VUW FAD35 <VUW_FAD35@myvuw.ac.nz>
2026-01-30 20:27:24 +1300

    added platform config

---
+++ models/platform/platform.gd
@@ -0,0 +1,32 @@
+@tool
+extends Node2D
+
+@export_enum("None", "Blue", "Green") var platform_type := "None":
+	set(value):
+		platform_type = value
+		_update_platform()
+
+@export var size := Vector2(300, 100):
+	set(value):
+		size = value
+		_update_platform()
+
+func _ready() -> void:
+	_update_platform()
+
+func _update_platform():
+	if not is_inside_tree():
+		return
+	$StaticBody2D/ColorRect.size = size
+	$StaticBody2D/ColorRect.position = Vector2(-size.x / 2, -size.y / 2)
+	print(platform_type)
+	
+	match platform_type:
+		"None":
+			$StaticBody2D/ColorRect.color = Color.WHITE
+		"Blue":
+			$StaticBody2D/ColorRect.color = Color.BLUE
+		"Green":
+			$StaticBody2D/ColorRect.color = Color.GREEN
+	
+	$StaticBody2D/CollisionShape2D.shape.size = size
