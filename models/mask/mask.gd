76e5bcc29e23d33227d93d968a649dab014a3f36
Thomas Page <thomaspage33@gmail.com>
2026-01-31 13:31:58 +1300

    update sprite of masks

--- models/mask/mask.gd
+++ models/mask/mask.gd
@@ -13,23 +13,24 @@ func _ready():
 	})
 
 func _on_reload_state():
-	$ColorRect.visible = not GameManager.get_object_state(get_instance_id(), "collected")
+	$Sprite2D.visible = not GameManager.get_object_state(get_instance_id(), "collected")
 		
 func _update_mask_type(mask: Types.Mask):
-	match mask:
-		Types.Mask.NONE:
-			$ColorRect.visible = false
-		Types.Mask.BLUE:
-			$ColorRect.visible = true
-			$ColorRect.color = "#0000ff"
-		Types.Mask.RED:
-			$ColorRect.visible = true
-			$ColorRect.color = "#ff0000"
+	pass
+	#match mask:
+		#Types.Mask.NONE:
+			#$Sprite2D.visible = false
+		#Types.Mask.BLUE:
+			#$Sprite2D.visible = true
+			#$Sprite2D.color = "#0000ff"
+		#Types.Mask.RED:
+			#$Sprite2D.visible = true
+			#$Sprite2D.color = "#ff0000"
 			
 
 func _on_body_entered(body: Node2D) -> void:
-	if $ColorRect.visible:
+	if $Sprite2D.visible:
 		print("Picked up " + str(mask))
 		GameManager.pickup_mask(mask)
 		GameManager.save_object_state(get_instance_id(), { "collected": true })
-		$ColorRect.visible = false
+		$Sprite2D.visible = false
